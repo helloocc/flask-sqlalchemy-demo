851b0387a629f2e6e93c70f24f7ebdc513958afc
Xixi Zhao <helloocc@users.noreply.github.com>
2018-04-03 00:56:33 +0800

    Update db.sql

--- examples/db.sql
+++ examples/db.sql
@@ -1,1 +1,1 @@
-create table todos (title VARCHAR(100) , text VARCHAR(100), done INT, pub_date DATE);
+create table todos (todo_id Int NOT NULL auto_increment,title VARCHAR(100) , text VARCHAR(100), done INT, pub_date DATETIME,PRIMARY KEY (todo_id)); 
